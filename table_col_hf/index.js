60e09372b3873637de5b75b9f439c88fb371482d
Schmidt Benedek Richard <schmidt.benedek@BOLYAI.LOCAL>
2025-10-03 13:08:37 +0200

    Felkesz

---
+++ table_col_hf/index.js
@@ -0,0 +1,60 @@
+/**
+ * @type {{theme: string, time: string, scientist1: string, scientist2: string}[]}
+ */
+
+const arr = [
+    {
+        theme: 'Optika',
+        time: 'XI. szazad',
+        scientist1: 'Alhazen'
+    },
+    {
+        theme: 'Asztronómia',
+        time: 'reneszánsz',
+        scientist1: 'Kepler',
+        scientist2: 'Galilei'
+    },
+    {
+        theme: 'Kvantumfizika',
+        time: 'XIX-XX. század',
+        scientist1: 'Max Planck',
+        scientist2: 'Albert Einstein'
+    },
+    {
+        theme: 'Modern fizika',
+        time: 'XX-XXI. század',
+        scientist1: 'Richard Feynman',
+        scientist2: 'Stephen Hawking'
+    }
+]
+
+const table = document.createElement("table")
+document.body.appendChild(table)
+
+const thead = document.createElement("thead") // Létrehozzuk a thead elementet
+table.appendChild(thead) //Hozzáfűzzük
+
+const tr = document.createElement("tr")
+thead.appendChild(tr)
+
+const th1 = document.createElement("th")
+const th2 = document.createElement("th")
+const th3 = document.createElement("th")
+tr.appendChild(th1)
+tr.appendChild(th2)
+tr.appendChild(th3)
+
+th1.innerText = "Fizika területe"
+th2.innerText = "Időszak"
+th3.innerText = "Képviselők"
+
+th3.colSpan = 2
+
+const tbody = document.createElement("tbody")
+table.appendChild(tbody)
+
+for(const x of arr){
+    const tr = document.createElement("tr")
+    tbody.appendChild(tr)
+}
+
